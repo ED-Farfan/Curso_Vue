0f58382e58f9750f99ce47b2f73fea1865ae9678
ED-Farfan <david1.dr86@gmail.com>
2020-09-08 11:59:59 -0500

    Modificamos el contenido del array 'Frutas' que eran string y los modificamos  a objetos

--- codigo_js.js
+++ codigo_js.js
@@ -2,6 +2,11 @@ const app = new Vue({
     el: '#app', // 'el' es propio de Vue, '#app' es el id del contenedor
     data:{ // Trabajamos con los datos
         titulo: 'Hola mundo con Vue', // Titulo es un objeto y el texto es su propiedad
-        frutas: ["Manzana",'Pera','Mango','Uva']
+        frutas: [
+            {nombre:"Manzana", cantidad:1},
+            {nombre:'Pera', cantidad:2},
+            {nombre:'Mango', cantidad:0},
+            {nombre:'Uva', cantidad:10}
+        ]
     }
 })

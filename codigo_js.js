91ce00fe88c04f4f86aff92c7e7e639b39769c94
ED-Farfan <david1.dr86@gmail.com>
2020-09-08 11:36:57 -0500

    Escribimos desde Js a HTML con Vue

---
+++ codigo_js.js
@@ -0,0 +1,6 @@
+const app = new Vue({
+    el: '#app', // 'el' es propio de Vue, '#app' es el id del contenedor
+    data:{ // Trabajamos con los datos
+        titulo: 'Hola mundo con Vue' // Titulo es un objeto y el texto es su propiedad
+    }
+})

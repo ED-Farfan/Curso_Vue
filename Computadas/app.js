392b75b9a1ec2b17456c34053eb96b460afe1415
ED-Farfan <david1.dr86@gmail.com>
2020-09-09 18:31:55 -0500

    Se agrego una barra de progreso de Bootstrap

--- Computadas/app.js
+++ Computadas/app.js
@@ -1,7 +1,8 @@
 const app = new Vue({
     el:"#app",
     data:{
-        mensaje: "Hola soy Far"
+        mensaje: "Hola soy Far",
+        contador: 0
     },
     computed:{
         invertido(){

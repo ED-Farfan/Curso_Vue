76f9c48313891244b77a3c2b003540d677ae0719
ED-Farfan <david1.dr86@gmail.com>
2020-09-10 19:43:26 -0500

    Se agrego un comunicacion estatica entre el hijo y el padre

--- Componentes/Componente/Componente_Hijo.js
+++ Componentes/Componente/Componente_Hijo.js
@@ -5,7 +5,18 @@ Vue.component("hijo", {
         <h5>Yo soy hijo</h5>
         <h5>Papá me mando esticamente un numero: {{numero_estatico}}</h5>
         <h5>Papá me mando un numero: {{numero_padre}}</h5>
+        Yo soy dueño de {{numeros}}
+        <button @click='numeros++'>+</button>
+
     </div>
     `,
-    props:['numero_estatico','numero_padre']
+    props:['numero_estatico','numero_padre'],
+    data(){
+        return{
+            numeros:10
+        }        
+    },
+    mounted(){
+        this.$emit('num_hijo',this.numeros)
+    }
 });
--- Componentes/Componente/Componente_Padre.js
+++ Componentes/Componente/Componente_Padre.js
@@ -4,16 +4,23 @@ Vue.component('Padre',{
     <div class="p-5 bg-dark text-white">
         <h4>{{papa}}</h4>
         <hr>
-        <h4>Yo mande un numero {{numero}}</h4>
+        <h4>Yo mande un numero {{numero}}</h4>        
+
+        Hijo mando el numero {{num_hi}}
+        <br>
         <button @click='numero++'>+</button>
-        <hijo numero_estatico='5' :numero_padre=numero ></hijo>
+
+        <hijo numero_estatico='5' :numero_padre=numero @num_hijo="num_hi = $event"></hijo>
+        
+
     </div>
     `,
     data(){
         return{
             papa: "Yo soy el padre",
             num : 0,
-            numero : 0
+            numero : 0,
+            num_hi : 0
         }
     }
 })

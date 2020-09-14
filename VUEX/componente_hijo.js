5ce22b29e88eae64d971540f36ade5ecc42d07ab
ED-Farfan <david1.dr86@gmail.com>
2020-09-14 12:02:07 -0500

    Se agrego en componente hijo un mapMutations para traer los metodos de la store

--- VUEX/componente_hijo.js
+++ VUEX/componente_hijo.js
@@ -3,6 +3,15 @@ Vue.component('hijo',{
         `
         <div>
             <button @click="$store.commit('aumentar')">+ {{$store.state.numero}}</button>
+            <hr>
+            <span>Boton Que funciona con mapMutations:</span>
+            <button @click="aumentar">+ {{$store.state.numero}}</button>
+            <button @click="disminuir">- {{$store.state.numero}}</button>
+            
+
         </div>
-        `
+        `,
+    methods:{
+        ...Vuex.mapMutations(['aumentar','disminuir'])
+    }
 })

f68412c6e80976dafe05b01ca3f02eac14abb9c8
ED-Farfan <david1.dr86@gmail.com>
2020-09-14 11:43:57 -0500

    En el componente padre se agrego un computed pare retornar lo que tenemos en estore.sate.numero

--- VUEX/componente_padre.js
+++ VUEX/componente_padre.js
@@ -4,8 +4,14 @@ Vue.component('titulo',{
         <div>
             <h1>Numero:</h1>
             <h3>{{$store.state.numero}}</h3>
+            <h3> Numero desde Computado: {{numero}}</h3>
             <hijo></hijo>
         </div>
-        `
+        `,
+        computed:{
+            numero(){
+                return store.state.numero
+            }
+        }
     
 })

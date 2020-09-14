54ae7e57f821ec3ca00a68b4a7909ff05176c33b
ED-Farfan <david1.dr86@gmail.com>
2020-09-14 12:07:23 -0500

    Se agrego en Store un metodo para disminuir de uno en uno y otro disminuir con parametros, ambos los utilizamos en el componente hijo

--- VUEX/componente_hijo.js
+++ VUEX/componente_hijo.js
@@ -7,11 +7,12 @@ Vue.component('hijo',{
             <span>Boton Que funciona con mapMutations:</span>
             <button @click="aumentar">+ {{$store.state.numero}}</button>
             <button @click="disminuir">- {{$store.state.numero}}</button>
+            <button @click="disminuir_numero(2)"> {{$store.state.numero}} - 2</button>
             
 
         </div>
         `,
     methods:{
-        ...Vuex.mapMutations(['aumentar','disminuir'])
+        ...Vuex.mapMutations(['aumentar','disminuir','disminuir_numero'])
     }
 })

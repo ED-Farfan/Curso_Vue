cab0c400c9d53a4b42e8547e4eacbc63fc0c1eb6
ED-Farfan <david1.dr86@gmail.com>
2020-09-14 12:49:43 -0500

    Se creo en Store una funcion de llenar cursos, se agrego un actions y dentro del mismo se agrego un metodo de obtener cursos el cual va a leer un json y va a mandar a la funcion llenar cursos. Dentro del componente hijo se le mapeo la accion y la mutacion para que pudiera utilizarlas por medio de un boton

--- VUEX/componente_hijo.js
+++ VUEX/componente_hijo.js
@@ -8,11 +8,21 @@ Vue.component('hijo',{
             <button @click="aumentar">+ {{$store.state.numero}}</button>
             <button @click="disminuir">- {{$store.state.numero}}</button>
             <button @click="disminuir_numero(2)"> {{$store.state.numero}} - 2</button>
+            <hr>
+            <h2>Cursos</h2>
+            <button @click="obtenercursos">Obtener Cursos</button>
+            <ul v-for="item of curso">
+                <li>{{item.nombre}}</li>
+            </ul>
             
 
         </div>
         `,
+    computed:{        
+        ...Vuex.mapState(['numero','curso'])
+    },
     methods:{
-        ...Vuex.mapMutations(['aumentar','disminuir','disminuir_numero'])
+        ...Vuex.mapMutations(['aumentar','disminuir','disminuir_numero']),
+        ...Vuex.mapActions(['obtenercursos'])
     }
 })

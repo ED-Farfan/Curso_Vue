37e233a24ae398a99b610bce3382edfe893e1851
ED-Farfan <david1.dr86@gmail.com>
2020-09-17 11:07:02 -0500

    Se creo una nueva ruta la cual se llama Servicios

---
+++ Proyecto_Vue_CLI_1/Con_Rutas/vue_router_1/src/views/Servicios.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Esta es la ventana de Servicios</h1>
+        <hr>
+        <h2>Hola Mundo Vue</h2>
+    </div>    
+</template>
+<script>
+export default {
+    name: 'Servicios'
+
+}
+</script>

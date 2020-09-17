6fca8969a9af69a7c0bafad2b22d09b12945fcbd
ED-Farfan <david1.dr86@gmail.com>
2020-09-17 11:40:47 -0500

    Se agrego un componente el cual va a mostrar el id que nosotros incluimos en la ruta dinamica

---
+++ Proyecto_Vue_CLI_1/Con_Rutas/vue_router_1/src/components/Fotografia.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>Yo soy la fotografia: {{$route.params.id}}</h3><!--Indica el id -->
+    </div>
+</template>
+<script>
+export default {
+    name:'Fotografia'
+}
+</script>
--- Proyecto_Vue_CLI_1/Con_Rutas/vue_router_1/src/views/Galeria.vue
+++ Proyecto_Vue_CLI_1/Con_Rutas/vue_router_1/src/views/Galeria.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
         <h1>Galeria</h1>
+        <Fotografia></Fotografia>
+        <span>Modificar la ruta despues de Galeria/</span>
+        <br>
+        <span>Ejemplo:</span>
+        <br>
+        <span>Galeria/{{$route.params.id}}</span>
     </div>
 </template>
 <script>
+import Fotografia from '@/components/Fotografia.vue'
 export default {
-    name:'Galeria'
+    name:'Galeria',
+    components:{
+        Fotografia
+    }
 }
 </script>

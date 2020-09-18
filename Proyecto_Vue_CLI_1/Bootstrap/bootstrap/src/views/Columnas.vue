af85d92b4709dcb1341b11a2f50bff42ff1a54b0
ED-Farfan <david1.dr86@gmail.com>
2020-09-18 15:40:27 -0500

    Se una vista de columnas la cual son trabajasdas con Bootstrap

---
+++ Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Columnas.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mt-5">
+        <b-row>
+            <b-col cols="12" md="6" lg="3">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt fuga perspiciatis quidem vitae odit iste rerum libero autem? Sint at cupiditate perferendis ex eum suscipit quod dolorem quos, velit ipsam!</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="9">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt fuga perspiciatis quidem vitae odit iste rerum libero autem? Sint at cupiditate perferendis ex eum suscipit quod dolorem quos, velit ipsam!</p>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="12" md="6" lg="3">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt fuga perspiciatis quidem vitae odit iste rerum libero autem? Sint at cupiditate perferendis ex eum suscipit quod dolorem quos, velit ipsam!</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="9">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt fuga perspiciatis quidem vitae odit iste rerum libero autem? Sint at cupiditate perferendis ex eum suscipit quod dolorem quos, velit ipsam!</p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+export default {
+    name:'Columnas'
+}
+</script>

c46dd6b00ee54828edab639aff4bf268391e9e75
ED-Farfan <david1.dr86@gmail.com>
2020-09-18 17:50:32 -0500

    Agregamos varias tarjetas

--- Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Cards.vue
+++ Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Cards.vue
@@ -13,6 +13,30 @@
         <b-button variant="outline-dark">Button</b-button>
         </b-card>
       </b-col>
+      <b-col cols="12" md="4">
+        <b-card title="Titulo Tarjeta" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae minima molestias amet magnam architecto aliquid! Doloremque unde mollitia soluta sunt, corrupti vitae quidem qui laboriosam libero cupiditate odit reprehenderit dicta.</p>
+        <b-button variant="outline-dark">Button</b-button>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card title="Titulo Tarjeta" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae minima molestias amet magnam architecto aliquid! Doloremque unde mollitia soluta sunt, corrupti vitae quidem qui laboriosam libero cupiditate odit reprehenderit dicta.</p>
+        <b-button variant="outline-dark">Button</b-button>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card title="Titulo Tarjeta" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae minima molestias amet magnam architecto aliquid! Doloremque unde mollitia soluta sunt, corrupti vitae quidem qui laboriosam libero cupiditate odit reprehenderit dicta.</p>
+        <b-button variant="outline-dark">Button</b-button>
+        </b-card>
+      </b-col>
+      <b-col cols="12" md="4">
+        <b-card title="Titulo Tarjeta" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae minima molestias amet magnam architecto aliquid! Doloremque unde mollitia soluta sunt, corrupti vitae quidem qui laboriosam libero cupiditate odit reprehenderit dicta.</p>
+        <b-button variant="outline-dark">Button</b-button>
+        </b-card>
+      </b-col>
     </b-row>
   </div>
 </template>

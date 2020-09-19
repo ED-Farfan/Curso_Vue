8fa9f764a21df34302d88333c0b4dcf0e109b057
ED-Farfan <david1.dr86@gmail.com>
2020-09-18 19:41:47 -0500

    Agregamos un formulario de seleccion, el cual detecta si no se a seleccionado una opcion

--- Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Formulario.vue
+++ Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Formulario.vue
@@ -1,15 +1,22 @@
 <template>
 <div>
     <h1>Formulario</h1>
+    <hr>
     <form action="">
         <b-row>
-            <b-col md="6">
+            
+            <b-col md="4">
+                <h2>Caja</h2>
                 <b-form-input v-model="texto" placeholder="Ingresa tú nombre" :state="comprobar"></b-form-input>
                 <small>Ingrese al menos 4 carácteres</small>
                 <h6>Texto"{{texto}}"</h6>
 
             </b-col>
-            
+            <b-col md="4">
+                <h2>Seleccion</h2>
+                <b-form-select v-model="selected" :options="animales" :state="comprobar2"></b-form-select>
+                <p>Usted selecciono: {{selected}} <span v-if="(selected === null)">NADA</span> </p>
+            </b-col>
         </b-row>
     </form>
 </div>    
@@ -19,12 +26,22 @@ export default {
     name:'Formulario',
     data(){
         return{
-            texto:''
+            texto:'',
+            selected: null,
+            animales:[
+                {value: null, text:"Seleccione un animal"},
+                {value: "Perro", text:"Guau"},
+                {value: "Gato", text:"Miau"},
+                {value: "Pato", text:"Cuack"}
+            ]
         }
     },
     computed:{
         comprobar(){
             return this.texto.length > 4 ? true:false
+        },
+        comprobar2(){
+            return this.selected !== null ? true:false
         }
     }
 }

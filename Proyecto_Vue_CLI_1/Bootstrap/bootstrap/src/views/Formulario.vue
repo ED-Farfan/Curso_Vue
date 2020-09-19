ca738f2caed73127bcd026a95a77658f4104f474
ED-Farfan <david1.dr86@gmail.com>
2020-09-18 19:25:47 -0500

    Agregamos un formulario de caja para ingresar datos y validar que sean mayor a 4 caracteres

--- Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Formulario.vue
+++ Proyecto_Vue_CLI_1/Bootstrap/bootstrap/src/views/Formulario.vue
@@ -1,8 +1,31 @@
 <template>
-    <h1>Hola</h1>
+<div>
+    <h1>Formulario</h1>
+    <form action="">
+        <b-row>
+            <b-col md="6">
+                <b-form-input v-model="texto" placeholder="Ingresa tú nombre" :state="comprobar"></b-form-input>
+                <small>Ingrese al menos 4 carácteres</small>
+                <h6>Texto"{{texto}}"</h6>
+
+            </b-col>
+            
+        </b-row>
+    </form>
+</div>    
 </template>
 <script>
 export default {
     name:'Formulario',
+    data(){
+        return{
+            texto:''
+        }
+    },
+    computed:{
+        comprobar(){
+            return this.texto.length > 4 ? true:false
+        }
+    }
 }
 </script>

eae75c914af242cf0e74bf7759cb34e9a246ebc7
ED-Farfan <david1.dr86@gmail.com>
2020-09-21 18:31:08 -0500

    Se agrego un calendario con Vuetify

--- Proyecto_Vue_CLI_1/Vuefy/vuefy/src/views/Home.vue
+++ Proyecto_Vue_CLI_1/Vuefy/vuefy/src/views/Home.vue
@@ -1,18 +1,30 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template>  
+<div>
+ 
+  <v-layout :wrap="true">
+    <v-flex xs12>      
+      <v-card>
+        <v-date-picker v-model="picker"></v-date-picker>
+      </v-card>
+      <v-card color="error" dark>
+        <v-card-text class="display-1 text-center" >
+          $65.00
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</div>
+ 
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   
   }
 }
 </script>

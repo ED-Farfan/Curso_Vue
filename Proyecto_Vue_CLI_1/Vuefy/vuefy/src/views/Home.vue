8cc83874241da345098682202f1a5019676b92a1
ED-Farfan <david1.dr86@gmail.com>
2020-09-21 19:37:27 -0500

    Muestra el precio del dolar dependiendo del dia

--- Proyecto_Vue_CLI_1/Vuefy/vuefy/src/views/Home.vue
+++ Proyecto_Vue_CLI_1/Vuefy/vuefy/src/views/Home.vue
@@ -4,11 +4,11 @@
   <v-layout :wrap="true">
     <v-flex xs12>      
       <v-card dark class="mx-auto">
-        <v-date-picker v-model="fecha" full-width dark locale="es-mx" :min=minimo :max=hoy></v-date-picker>
+        <v-date-picker v-model="fecha" full-width dark locale="es-mx" :min=minimo :max=hoy @change="getDolar(fecha)"></v-date-picker>
       </v-card>
       <v-card color="error" dark >
         <v-card-text class="display-1 text-center" >
-          $65.00 --- {{fecha}}     {{hoy}}
+          ${{precio}}   Dia:{{fecha}}
         </v-card-text>
       </v-card>
     </v-flex>
@@ -19,17 +19,30 @@
 
 <script>
 // @ is an alias to /src
-
+import axios from 'axios'
 
 export default {
   name: 'Home',
   data(){
     return{
-      fecha:"",
+      fecha:new Date().toISOString().substr(0, 10),
       minimo:"1990",
-      hoy: new Date().toISOString().substr(0, 10)
+      hoy: new Date().toISOString().substr(0, 10),
+      precio:0.0
     }
 
+  },
+  methods: {
+    async getDolar(dia){      
+      console.log("DIA");
+      console.log(dia)
+      let datos = await axios.get(`https://mindicador.cl/api/dolar/${dia}`)
+      
+      this.precio = await datos.data.serie[0].valor
+    }
+  },
+  created() {
+    this.getDolar(this.hoy)
   },
   components: {
    

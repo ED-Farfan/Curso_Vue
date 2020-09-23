97f41871c19b335fcc44cc1ab6e19d7f72d2b471
ED-Farfan <david1.dr86@gmail.com>
2020-09-23 18:54:58 -0500

    Se creo y se agrego un componente Nav bar

---
+++ Proyecto_Vue_CLI_1/Vuefy/curso/src/components/Barra.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-toolbar dark dense src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" main>
+      <v-app-bar-nav-icon @click="drawe=!drawe"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-battery-alert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawe" temporary dark absolute>
+      <v-layout mt-4 aling-center colums wrap>
+        <v-flex xs12>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-flex>
+        <v-flex>
+            <p class="white--text mt-3 headline">Juan Perez</p>
+        </v-flex>
+      </v-layout>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  name: "Barra",
+  data() {
+    return {
+      drawe: true,
+    };
+  },
+};
+</script>

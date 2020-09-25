e1a004a40e53710bea8beba443bc54ca5b6b101d
ED-Farfan <david1.dr86@gmail.com>
2020-09-25 12:55:23 -0500

    Agregamos un dialog

--- Proyecto_Vue_CLI_1/Vuefy/curso/src/views/Cards.vue
+++ Proyecto_Vue_CLI_1/Vuefy/curso/src/views/Cards.vue
@@ -2,7 +2,7 @@
   <v-container grid-list-md>
     <v-layout row wrap>
       <v-flex xs12 sm6>
-        <v-card dark > 
+        <v-card :dark="true" > 
             <v-card-title primary-title  class="justify-center">
                 Titulo
             </v-card-title>
@@ -35,15 +35,50 @@
             </div>
           </v-card-text>
           <v-card-actions>
-            <v-btn text color=""> Hola </v-btn>
+            <v-btn text color="primary" @click="dialog=true"> Dialog </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
+    <v-dialog
+        v-model="dialog"
+        scrollable  
+        persistent         
+        transition="dialog-transition"
+    >
+        <v-card>
+            <v-card-title primary-title>
+                Hola
+            </v-card-title>
+                <v-divider                  
+                ></v-divider>
+            <v-card-text>              
+              <div class="text--primary">
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic architecto ullam cupiditate at dolorum praesentium dolore cumque quasi cum culpa. Eaque quis voluptate veniam. Consequatur provident voluptates quo totam neque.
+                </p>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="black"
+                @click="dialog=false"
+              >
+               cerrar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+    </v-dialog>
   </v-container>
 </template>
 <script>
 export default {
   name: "Cards",
+  data() {
+      return {
+          dialog: true
+      }
+  },
 };
 </script>

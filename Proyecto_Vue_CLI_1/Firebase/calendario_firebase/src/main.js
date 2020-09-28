ef21140d5c6f175465ccc22498aba2608604d4b0
ED-Farfan <david1.dr86@gmail.com>
2020-09-28 11:32:07 -0500

    Se borro el contenido de home y se agrego un calendario

--- Proyecto_Vue_CLI_1/Firebase/calendario_firebase/src/main.js
+++ Proyecto_Vue_CLI_1/Firebase/calendario_firebase/src/main.js
@@ -6,6 +6,25 @@ import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
+import VueTextareaAutosize from 'vue-textarea-autosize'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWWGPvLus-mcyddqifccdh-cbjAJyKllE",
+  authDomain: "vue-calendario-2020.firebaseapp.com",
+  databaseURL: "https://vue-calendario-2020.firebaseio.com",
+  projectId: "vue-calendario-2020",
+  storageBucket: "vue-calendario-2020.appspot.com",
+  messagingSenderId: "458599853772",
+  appId: "1:458599853772:web:2c2651e6454b5c27201c69"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore()
 Vue.config.productionTip = false
 
 new Vue({

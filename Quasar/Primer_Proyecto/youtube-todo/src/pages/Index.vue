77105a555f4561d542471416bd4850be8e913b7b
ED-Farfan <david1.dr86@gmail.com>
2020-10-14 12:55:06 -0500

    Agregamos un boton en el navar de editor

--- Quasar/Primer_Proyecto/youtube-todo/src/pages/Index.vue
+++ Quasar/Primer_Proyecto/youtube-todo/src/pages/Index.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-editor v-model="editor" min-height="5rem" />
+    <q-editor
+      v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Save your work',
+          icon: 'save',
+          label: 'Save',
+          handler: saveWork
+        }
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']
+      ]"
+    />
 
     
 
@@ -16,6 +30,16 @@ export default {
   data() {
     return {
       editor: '',
+    }
+  },
+  methods: {
+    saveWork () {
+      this.$q.notify({
+        message: 'Saved your text to local storage',
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'cloud_done'
+      })
     }
   },
 }
